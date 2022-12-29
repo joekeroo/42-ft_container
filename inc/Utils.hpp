4a1cb3456bdf1d607d12b18ed69acbc551cd1d7c
Joel king yan Hii <jhii@u82z03s03.42kl.edu.my>
2022-12-29 15:27:59 +0800

    ft::vector

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:32:38 by jhii              #+#    #+#             */
-/*   Updated: 2022/12/23 16:17:02 by jhii             ###   ########.fr       */
+/*   Updated: 2022/12/29 15:17:23 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@
 # include <iostream>
 # include "Vector.hpp"
 
+void	testStack(void);
 void	testVector(void);
 void	testVectorAll(void);
 void	testVectorConstructor(void);
