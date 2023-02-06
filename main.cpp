fd8b8fcae7166972e3f2fb2110e57a767e43cdda
Joel king yan Hii <jhii@u91z05s05.42kl.edu.my>
2023-02-06 12:55:48 +0800

    modified main.cpp to check for leaks

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:24:58 by jhii              #+#    #+#             */
-/*   Updated: 2023/01/10 15:15:35 by jhii             ###   ########.fr       */
+/*   Updated: 2023/02/06 12:51:38 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	main(void)
 	testVector();
 	testStack();
 	testRBT();
+	system("leaks ft_container");
 	return (0);
 }
