b81d23ac732b453b91d842d8973b1ab352c075a0
Joel king yan Hii <jhii@u91z05s05.42kl.edu.my>
2023-02-06 15:54:43 +0800

    added 'if' statement to toggle between printing leaks and no leaks

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:24:58 by jhii              #+#    #+#             */
-/*   Updated: 2023/02/06 12:51:38 by jhii             ###   ########.fr       */
+/*   Updated: 2023/02/06 15:53:32 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 #include "Stack.hpp"
 #include "RedBlackTree.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	testVector();
 	testStack();
 	testRBT();
-	system("leaks ft_container");
+	if (argc == 2)
+	{
+		std::string	str(argv[1]);
+		if (str == "leaks")
+			system("leaks ft_container");
+	}
 	return (0);
 }
